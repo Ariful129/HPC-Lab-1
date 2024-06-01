82980ae63c35fcc283ee9ed90fc25a4689da1ef3
Ariful129 <u1904129@student.cuet.ac.bd>
2024-06-02 01:42:56 +0600

    Second commit

--- src/Bank.java
+++ src/Bank.java
@@ -1,62 +1,60 @@
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Bank {
     String accountNumber;
-
     OperationsQueue operationsQueue;
-
     int balance = 0;
+    private final Lock lock = new ReentrantLock();
 
     public Bank(String accountNumber, OperationsQueue operationsQueue) {
         this.accountNumber = accountNumber;
         this.operationsQueue = operationsQueue;
     }
 
-    // A deposit function that will run in parallel on a separate thread. It will be a loop where in each iteration, it read the amount from the operationQueue and deposit the amount.
     public void deposit() {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         while (true) {
             int amount = operationsQueue.getNextItem();
-            if(amount == -9999) {
-                //operationsQueue.add(-9999);
+            if (amount == -9999) {
                 break;
             }
-            if (amount>0) {
-                balance =  balance + amount;
-                System.out.println("Deposited: " + amount + " Balance: " + balance);
-            }
-            else{
+            if (amount > 0) {
+                lock.lock();
+                try {
+                    balance += amount;
+                    System.out.println("Deposited: " + amount + " Balance: " + balance);
+                } finally {
+                    lock.unlock();
+                }
+            } else {
                 operationsQueue.add(amount);
-                System.out.println("operation added back "+amount);
+                System.out.println("Operation added back " + amount);
             }
-
         }
     }
 
-    // A withdraw function that will run in parallel on a separate thread. It will be a loop where in each iteration, it read the amount from the operationQueue and withdraw the amount.
     public void withdraw() {
         while (true) {
             int amount = operationsQueue.getNextItem();
-            if(amount == -9999) {
-               // operationsQueue.add(-9999);
+            if (amount == -9999) {
                 break;
             }
-
-            if(balance+amount<0){
-
-                System.out.println("Not enough balance to deposite "+amount);
-                continue;
+            lock.lock();
+            try {
+                if (balance + amount < 0) {
+                    System.out.println("Not enough balance to withdraw " + amount);
+                    continue;
+                }
+                if (amount < 0) {
+                    balance += amount;
+                    System.out.println("Withdrawn: " + amount + " Balance: " + balance);
+                } else {
+                    operationsQueue.add(amount);
+                    System.out.println("Operation added back " + amount);
+                }
+            } finally {
+                lock.unlock();
             }
-
-            if (amount<0) {
-                balance =  balance + amount;
-                System.out.println("Withdrawn: " + amount + " Balance: " + balance);
-            }
-            else{
-                operationsQueue.add(amount);
-                System.out.println("operation added back "+amount);
-            }
-
         }
     }
 }
